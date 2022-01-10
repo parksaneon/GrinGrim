a0138b4e2af30235099417e873742d23e47133e2
parksaneon <sangeon1500@daum.net>
2022-01-10 19:28:05 +0900

    build environment

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/style.scss';
+import "../scss/style.scss";
 
 const pow = 3.14;
 const sum = (a, b) => a + b;
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,44 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: "./src/js/index.js",
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js"
   },
   module: {
     rules: [
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
   },
   devServer: {
     port: 9000,
-    liveReload: true,
+    liveReload: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: "index.html"
     }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "index.css"
+    })
   ],
-  mode: 'development',
+  mode: "development"
 };
