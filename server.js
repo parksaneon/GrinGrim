2eb8607cf3b940f7ff8eb11c010e1e1e8a849bda
Eunseong, Yeo <e7838752@gmail.com>
2022-01-11 18:53:37 +0900

    ✨ feat: create mydrawings client and server
    나의 그림을 보여줄 수 있는 기능 구현

--- server.js
+++ server.js
@@ -1,16 +1,57 @@
-import express from "express";
-import cors from "cors";
-import userRouter from "./router/userRouter.js";
+import express from 'express';
+import cors from 'cors';
+import userRouter from './router/userRouter.js';
 
 const app = express();
 const port = 8000;
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(cors());
+const users = [
+  {
+    id: 100,
+    nickname: '최한나'
+  },
+  {
+    id: 10,
+    nickname: '박상언'
+  }
+];
 
-app.use("/users", userRouter);
+const drawings = [
+  {
+    id: 1,
+    userid: 100,
+    url: '/images/drawings/test.jpeg',
+    category: '정신',
+    likedUserId: [1, 2, 3, 4]
+  },
+  {
+    id: 2,
+    userid: 10,
+    url: '/images/drawings/test.jpeg',
+    category: '정신',
+    likedUserId: [1, 2, 5]
+  },
+  {
+    id: 3,
+    userid: 10,
+    url: '/images/drawings/test.jpeg',
+    category: '잃음',
+    likedUserId: [1, 2, 3, 4, 5]
+  }
+];
+
+// app.use(express.static('public'));
+app.use('/images', express.static('images'));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+
+app.get('/drawings/:userid', (req, res) => {
+  const { userid } = req.params;
+  const drawingsFilterByUserId = drawings.filter(drawing => drawing.userid === +userid);
+  res.send(drawingsFilterByUserId);
+});
 
 app.listen(port, () => {
-  console.log("hi2");
+  console.log('hi2');
 });
