744dfd998467931ce25c5f585a6a8b66a5cce109
Eunseong, Yeo <e7838752@gmail.com>
2022-01-11 19:30:00 +0900

    ♻️ refator: Create nickname
    작성자 닉네임을 서버에서 함께 전송

--- src/js/mydrawings.js
+++ src/js/mydrawings.js
@@ -9,10 +9,11 @@ const render = async () => {
   const { data: mydrawings } = await axios.get(`http://localhost:8000/drawings/${USER_ID}`);
   root.innerHTML = mydrawings
     .map(
-      ({ id, url, likedUserId }) => `
+      ({ id, url, likedUserId, nickname }) => `
       <div data-id="${id}">
         <img src="http://localhost:8000${url}">
         <p>좋아요 수: ${likedUserId.length}</p>
+        <p>작성자: ${nickname}</p>
       </div>
     `
     )
