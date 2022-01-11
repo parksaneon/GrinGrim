c1926d056856287e4e6fb2381c2940863545d755
hann3 <soyo_0131@naver.com>
2022-01-11 23:43:57 +0900

    ✨ feat: upload canvas image to server

--- server.js
+++ server.js
@@ -1,11 +1,58 @@
-const express = require("express");
+import express from 'express';
+import cors from 'cors';
+// import userRouter from './router/userRouter.js';
+
+import multer from 'multer';
 
 const app = express();
 const port = 8000;
 
-app.use(express.static("public"));
+app.use('/images', express.static('images'));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
 
+let drawings = [];
+
+const categories = [
+  {
+    id: 1,
+    name: '기린'
+  },
+  {
+    id: 2,
+    name: '퇴근'
+  }
+];
+
+const upload = multer({
+  limits: { fileSize: 5 * 1024 * 1024 },
+  storage: multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, 'images/drawings/');
+    },
+    filename(req, file, cb) {
+      cb(null, file.originalname);
+    }
+  })
+});
+
+const generateId = data => (data[data.length - 1]?.id || 0) + 1;
+
+app.post('/drawings', upload.single('file'), (req, res) => {
+  console.log('UPLOAD SUCCESS!', req.file);
+  drawings = [
+    ...drawings,
+    {
+      id: generateId(drawings),
+      userid: 1,
+      url: req.file.destination + req.file.originalname,
+      categoryid: 1,
+      likedUserId: []
+    }
+  ];
+});
+
 app.listen(port, () => {
-  console.log("hi2");
+  console.log('hi2');
 });
--- src/js/draw.js
+++ src/js/draw.js
@@ -1,3 +1,6 @@
+import regenerato from 'regenerator-runtime';
+import axios from 'axios';
+
 const $canvas = document.querySelector('.my-canvas');
 const ctx = $canvas.getContext('2d');
 
@@ -26,4 +29,29 @@ const finishDrawing = () => {
 $canvas.addEventListener('mousemove', draw);
 $canvas.addEventListener('mousedown', startDrawing);
 $canvas.addEventListener('mouseup', finishDrawing);
-$canvas.addEventListener('mouseleave', finishDrawing);
+
+const convertToBlob = base64 => {
+  const decodImg = atob(base64.split(',')[1]);
+  const array = [];
+  for (let i = 0; i < decodImg.length; i++) {
+    array.push(decodImg.charCodeAt(i));
+  }
+  return new Blob([new Uint8Array(array)], {
+    type: 'image/png'
+  });
+};
+
+const uploadCanvas = async () => {
+  const image = $canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+  const file = convertToBlob(image);
+  const fileName = 'canvas' + new Date().getTime() + '.png';
+  const formData = new FormData();
+  formData.append('file', file, fileName);
+
+  axios.post('http://localhost:8000/drawings', formData, {
+    processData: false,
+    contentType: false
+  });
+};
+
+document.querySelector('.save').addEventListener('click', uploadCanvas);
