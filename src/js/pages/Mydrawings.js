e4e33d6bc2136ac03c3c9997547a70e01b7e4e5c
Eunseong, Yeo <e7838752@gmail.com>
2022-01-18 19:30:18 +0900

    ✨ feat: add category name to my drawings
    내 그림에 카테고리 아이디를 받아오는 코드 구현
    promise.all로 비동기 처리 구현

--- src/js/pages/Mydrawings.js
+++ src/js/pages/Mydrawings.js
@@ -1,17 +1,32 @@
 import axios from 'axios';
 
 export default () => ({
-  getData() {
-    return axios.get(`/drawings/userid/${1}`);
+  async getData() {
+    const USER_ID = 1;
+    const { data: mydrawings } = await axios.get(`/drawings/userid/${USER_ID}`);
+    const categoryNamePromise = mydrawings.map(({ categoryId }) => axios.get(`/category/${categoryId}/name`));
+
+    const categoryNames = await Promise.all(categoryNamePromise);
+
+    let mydrawingsWithCategoryName = [];
+
+    mydrawings.forEach((mydrawing, index) => {
+      mydrawingsWithCategoryName = [
+        ...mydrawingsWithCategoryName,
+        { ...mydrawing, categoryName: categoryNames[index].data }
+      ];
+    });
+
+    return { data: mydrawingsWithCategoryName };
   },
 
   getHtml(mydrawings) {
     return mydrawings
       .map(
-        ({ id, categoryid, likedUserId, url, nickname }) => `
+        ({ id, categoryName, likedUserId, url, nickname }) => `
             <img src="${url}">
             <div>아이디: ${id}</div>
-            <div>카테고리 아이디: ${categoryid}</div>
+            <div>카테고리: ${categoryName}</div>
             <div>좋아요: ${likedUserId.length}</div>
             <div>닉네임: ${nickname}</div>
       `
