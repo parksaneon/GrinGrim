9ce9d85566ed1ad5bc6e4818c6870415e8587a75
hann3 <soyo_0131@naver.com>
2022-01-18 14:38:05 +0900

    🚧 WIP: add my drawing page files

--- src/js/pages/Mydrawings.js
+++ src/js/pages/Mydrawings.js
@@ -23,26 +23,3 @@ export default () => ({
     return null;
   }
 });
-
-// export default class {
-//   constructor() {
-//     document.title = 'Mydrawings';
-//   }
-
-//   async getHtml() {
-//     const USER_ID = 10;
-//     const { data: mydrawings } = await axios.get(`/drawings/userid/${USER_ID}`);
-
-//     return mydrawings
-//       .map(
-//         ({ id, categoryid, likedUserId, url, nickname }) => `
-//         <img src="${url}">
-//         <div>아이디: ${id}</div>
-//         <div>카테고리 아이디: ${categoryid}</div>
-//         <div>좋아요: ${likedUserId.length}</div>
-//         <div>닉네임: ${nickname}</div>
-//   `
-//       )
-//       .join('');
-//   }
-// }
