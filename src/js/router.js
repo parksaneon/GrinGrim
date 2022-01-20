fbc9bba60088d93a1afe89f0edc58784d4d4476e
Eunseong, Yeo <e7838752@gmail.com>
2022-01-20 11:29:36 +0900

    ✨ feat: add unauthorized component
    권한 업는 접속 페이지 컴포넌트 추가

--- src/js/router.js
+++ src/js/router.js
@@ -4,6 +4,7 @@ import Ranking from './pages/Ranking.js';
 import Draw from './pages/Draw.js';
 import Result from './pages/Result.js';
 import NotFound from './pages/NotFound.js';
+import Unauthorized from './pages/Unauthorized.js';
 
 const routes = [
   { path: '/', component: Home },
@@ -11,6 +12,7 @@ const routes = [
   { path: '/ranking', component: Ranking },
   { path: '/draw', component: Draw },
   { path: '/result', component: Result },
+  { path: '***', component: Unauthorized },
   { path: '**', component: NotFound }
 ];
 
@@ -20,7 +22,7 @@ const getComponent = path =>
 const render = async (el, path, query) => {
   const page = getComponent(path);
   const { data } = await page.getData(query);
-  el.innerHTML = await page.getHtml(data);
+  el.innerHTML = page.getHtml(data);
   page.eventBinding(el);
 };
 
